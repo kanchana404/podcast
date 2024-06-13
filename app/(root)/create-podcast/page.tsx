6d1a872d027419ed23a75fc6e0f39d4b7a497d02
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-13 19:45:19 +0530

    chore: Update layout and provider components

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CreatePodcast = () => {
   return (
-    <div>
+    <section>
       <h1 className='text-20 font-bold text-white-1'>CreatePodcast</h1>
-    </div>
+    </section>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,7 @@ import { api } from "@/convex/_generated/api";
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
         <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
     </div>
 
        <div className="podcast_grid">
