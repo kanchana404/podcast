3ed55ff0b791fd0381d6f35aa6a34e025e8c9049
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-16 01:30:52 +0530

    feat: Add lazy loading for podcast images and update view count tracking

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,71 +1,58 @@
-"use client";
+'use client';
 
-import { sidebarLinks } from "@/constants";
-import { cn } from "@/lib/utils";
-import { SignedIn, SignedOut, useClerk, useUser } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-import { Button } from "./ui/button";
-import Router from "next/router";
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs';
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import React from 'react'
+import { Button } from './ui/button';
+import { useAudio } from '@/providers/AudioProvider';
 
 const LeftSidebar = () => {
   const pathname = usePathname();
-
-  const {signOut} = useClerk();
+  const router = useRouter();
+  const { signOut } = useClerk();
+  const { audio } = useAudio();
 
   return (
-    <section className="left_sidebar">
+    <section className={cn("left_sidebar h-[calc(100vh-5px)]", {
+      'h-[calc(100vh-140px)]': audio?.audioUrl
+    })}>
       <nav className="flex flex-col gap-6">
-        <Link
-          href="/"
-          className="flex cursor-pointer items-center gap-1 pd-10 mx-lg:justify-center"
-        >
+        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
-            Podcaster
-          </h1>
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">Podcastr</h1>
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
-          const isActive =
-            pathname === route || pathname.startsWith(`${route}/`);
+          const isActive = pathname === route || pathname.startsWith(`${route}/`);
 
-          return (
-            <Link
-              href={route}
-              key={label}
-              className={cn(
-                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
-                {
-                  "bg-nav-focus border-r-4 border-orange-1": isActive,
-                } // Add a class or style for active links if needed
-              )}
-            >
-              <Image src={imgURL} alt={label} width={24} height={24} />
-              <p>{label}</p>
-            </Link>
-          );
+          return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+            'bg-nav-focus border-r-4 border-orange-1': isActive
+          })}>
+            <Image src={imgURL} alt={label} width={24} height={24} />
+            <p>{label}</p>
+          </Link>
         })}
-      </nav>
-
+      </nav>  
       <SignedOut>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button className="text-16 w-full bg-orange-1 font-extrabold" asChild>
-            <Link href="/sign-in">Sign In</Link>
+          <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
+            <Link href="/sign-in">Sign in</Link>
           </Button>
         </div>
       </SignedOut>
       <SignedIn>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button className="text-16 w-full bg-orange-1 font-extrabold" onClick={() =>signOut(() => Router.push('/'))}>
-           Logout
+          <Button className="text-16 w-full bg-orange-1 font-extrabold" onClick={() => signOut(() => router.push('/'))}>
+            Log Out
           </Button>
         </div>
       </SignedIn>
     </section>
-  );
-};
+  )
+}
 
-export default LeftSidebar;
+export default LeftSidebar
