2fef983a6ead78aacd99dfd0ebe9f9b45f2abe55
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-15 10:58:43 +0530

    chore: Update RightSidebar component with user profile link and header section

---
+++ components/Carsoul.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Carsoul = () => {
+  return (
+    <div>Carsoul</div>
+  )
+}
+
+export default Carsoul
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client";
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Header from "./Header";
+import Carsoul from "./Carsoul";
 
 const RightSidebar = () => {
-  return (
-   <section className='right_sidebar text-white-1'>
-RightSidebar
-   </section>
-  )
-}
+  const { user } = useUser();
 
-export default RightSidebar
+  return (
+    <section className="right_sidebar text-white-1">
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">
+              {user?.firstName} {user?.lastName}
+            </h1>
+            <Image
+              src="/icons/right-arrow.svg"
+              width={24}
+              height={24}
+              alt="right arrow"
+            />
+          </div>
+        </Link>
+      </SignedIn>
+
+      <section>
+        <Header />
+        <Carsoul />
+      </section>
+    </section>
+  );
+};
+
+export default RightSidebar;
